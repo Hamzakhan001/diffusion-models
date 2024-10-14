b14acc8131a0ed2c501adbbf06923ef5055afe47
hamzakhanmpn <163988551+hamzakhanmpn@users.noreply.github.com>
2024-10-14 23:01:33 +0100

    mobile screen

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -1,25 +1,25 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import {
-  Sheet,
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { SignedIn, UserButton } from "@clerk/nextjs";
+import { navLinks } from "../../constants";
 
 const MobileNav = () => {
+  const pathName = usePathname();
   return (
     <header className="header">
       <Link href="/" className="flex items-center gap-2 md:py-2">
-        <Image src="/assets/images/logo-text.svg" width={180} height={28} />
+        <Image
+          src="/assets/images/logo-text.svg"
+          width={180}
+          height={28}
+          alt="logo"
+        />
       </Link>
 
       <nav className="flex gap-2">
@@ -27,12 +27,45 @@ const MobileNav = () => {
           <UserButton />
           <Sheet>
             <SheetTrigger>
-              <Image src="/assets/icons/menu.svg" alt="menu" width={32} height={32} className="cursor-pointer"/>
+              <Image
+                src="/assets/icons/menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
+                className="cursor-pointer"
+              />
             </SheetTrigger>
             <SheetContent className="sheet-content sm:w-64">
               <>
-              <Image src="/assets/images/logo-text.svg" width={152} height={32} alt="logo"
-              />
+                <Image
+                  src="/assets/images/logo-text.svg"
+                  width={152}
+                  height={32}
+                  alt="logo"
+                />
+                <ul className="header_nav-elements">
+                  {navLinks.map((link) => {
+                    const isActive = link.route === pathName;
+                    console.log("us", isActive);
+                    return (
+                      <li
+                        key={link.route}
+                        className='p-18 flex whitespace-nowrap text-dark-700'
+                      >
+                        <Link className="sidebar-link" href={link.route}>
+                          <Image
+                            src={link.icon}
+                            alt="logo"
+                            width={24}
+                            height={24}
+                            className={`${isActive && "brightness-200"}`}
+                          />
+                          {link.label}
+                        </Link>
+                      </li>
+                    );
+                  })}
+                </ul>
               </>
             </SheetContent>
           </Sheet>
